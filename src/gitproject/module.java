94fb026d6737e2843b9bbbce7c055f3f47c798de
Mphostar <gift.masoga@gmail.com>
2016-03-30 11:07:58 +0200

    initial checking of gitproject

---
+++ src/gitproject/module.java
@@ -0,0 +1,13 @@
+package gitproject;
+
+public class module {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		subclass v = new subclass();
+		v.maths();
+
+	}
+
+}
---
+++ src/gitproject/subclass.java
@@ -0,0 +1,19 @@
+package gitproject;
+
+public class subclass {
+	void maths(){
+		int num1 = 2;
+		int num2 = 6;
+		int sum, average;
+		
+		sum = num1 + num2;
+		average = sum/2;
+		
+		System.out.println("sum is "+sum + "Averagw is "+ average);
+	}
+
+	
+		
+	}
+
+
